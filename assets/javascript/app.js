855e0a256639ddd5b48844b9246e914f9fba9bc4
MarycBrown02 <marycbrown01@sbcgloba.net>
2018-09-22 22:13:49 -0500

    modified 2 files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -83,6 +83,8 @@ correctAnswer++;
 }
 
 });
-document.write(questionOne);
+
+console.log(questionOne);
+$("#questions").prepend(questionOne);
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
